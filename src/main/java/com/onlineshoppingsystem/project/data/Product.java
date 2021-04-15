d6c00dbc25b0b7b7a6c510a91cd8c4b8deaa78bd
Paulinax33 <paulinka6105@onet.pl>
2021-04-16 00:04:24 +0200

    class user and product

--- src/main/java/com/onlineshoppingsystem/project/data/Product.java
+++ src/main/java/com/onlineshoppingsystem/project/data/Product.java
@@ -1,4 +1,83 @@
 package com.onlineshoppingsystem.project.data;
 
+import javax.persistence.Entity;
+
+@Entity
 public class Product {
+    private float price;
+    private int quantity;
+    private String type;
+    private String name;
+    private String group;
+    private String subgroup;
+    private int id;
+
+    Product(){
+
+    }
+    public Product(float price,int quantity,String type,String name,String group,String subgroup, int id){
+        this.price = price;
+        this.quantity = quantity;
+        this.type = type;
+        this.name = name;
+        this.group = group;
+        this.subgroup = subgroup;
+        this.id = id;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    public void setGroup(String group) {
+        this.group = group;
+    }
+
+    public String getSubgroup() {
+        return subgroup;
+    }
+
+    public void setSubgroup(String subgroup) {
+        this.subgroup = subgroup;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
--- src/main/java/com/onlineshoppingsystem/project/data/User.java
+++ src/main/java/com/onlineshoppingsystem/project/data/User.java
@@ -1,4 +1,40 @@
 package com.onlineshoppingsystem.project.data;
 
 public class User {
+    private long id;
+    private String username;
+    private String password;
+
+    User(){
+
+    }
+    public User(long id, String username, String password){
+        this.id = id;
+        this.username = username;
+        this.password = password;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
 }
