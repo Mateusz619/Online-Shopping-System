5e5c196766e28b7f43195e6c4fe718182f5f12be
Arkadiusz <arek1990cda@gmail.com>
2021-04-17 21:45:03 +0200

    add-cart-and-product-classes

--- src/main/java/com/onlineshoppingsystem/project/data/Cart.java
+++ src/main/java/com/onlineshoppingsystem/project/data/Cart.java
@@ -1,4 +1,26 @@
 package com.onlineshoppingsystem.project.data;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "cart")
+@NoArgsConstructor
 public class Cart {
-}
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private int quantity;
+    private double total;
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    public Cart(int quantity, double total, Product product) {
+        this.quantity = quantity;
+        this.total = total;
+        this.product = product;
+    }
--- src/main/java/com/onlineshoppingsystem/project/data/Customer.java
+++ src/main/java/com/onlineshoppingsystem/project/data/Customer.java
@@ -1,4 +1,29 @@
 package com.onlineshoppingsystem.project.data;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name="customer")
+@NoArgsConstructor
 public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    @OneToOne
+    @JoinColumn(name="user_id")
+    private User user;
+
+    public Customer(String firstName, String lastName, String phoneNumber, User user) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.user = user;
+    }
 }
