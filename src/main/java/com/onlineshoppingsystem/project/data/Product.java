7300b2c685b02e8e6bfecd036d602fe6661cbc0d
Paulinax33 <paulinka6105@onet.pl>
2021-04-18 13:23:29 +0200

    changes

--- src/main/java/com/onlineshoppingsystem/project/data/Product.java
+++ src/main/java/com/onlineshoppingsystem/project/data/Product.java
@@ -2,9 +2,10 @@ package com.onlineshoppingsystem.project.data;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import javax.persistence.*;
+import java.util.List;
 
 @Data
-@Entity
+@Entity(name = "Product")
 @NoArgsConstructor
 public class Product {
     @Id
@@ -15,16 +16,13 @@ public class Product {
     private int quantity;
     private String type;
     private String name;
-    private String group;
-    private String subgroup;
 
-    public Product(float price,int quantity,String type,String name,String group,String subgroup, int id){
+    public Product(float price, int quantity, String type, String name, int id){
         this.price = price;
         this.quantity = quantity;
         this.type = type;
         this.name = name;
-        this.group = group;
-        this.subgroup = subgroup;
         this.id = id;
+
     }
 }
--- src/main/java/com/onlineshoppingsystem/project/data/User.java
+++ src/main/java/com/onlineshoppingsystem/project/data/User.java
@@ -6,7 +6,7 @@ import lombok.NoArgsConstructor;
 import javax.persistence.*;
 
 @Data
-@Entity
+@Entity(name = "User")
 @NoArgsConstructor
 public class User {
     @Id
