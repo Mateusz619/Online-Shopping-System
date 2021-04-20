8b5b1dc87ed3ffd1ee231c8a9c4553005c8dd69e
Arkadiusz <arek1990cda@gmail.com>
2021-04-20 20:32:28 +0200

    create product controller, repository and dto, modify classes: prod, customer, projectApp

---
+++ src/main/java/com/onlineshoppingsystem/project/controller/ProductController.java
@@ -0,0 +1,33 @@
+package com.onlineshoppingsystem.project.controller;
+
+import com.onlineshoppingsystem.project.data.Product;
+import com.onlineshoppingsystem.project.dto.ProductDTO;
+import com.onlineshoppingsystem.project.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class ProductController {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @GetMapping(value = "product/{id}")
+    public ResponseEntity getProductById(@PathVariable Long id) {
+        return productRepository.findById(id)
+                .map(product -> ResponseEntity.ok(product))
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @GetMapping(value = "products")
+    public ResponseEntity getAllProducts() {
+        return ResponseEntity.ok(productRepository.findAll());
+    }
+
+    @PostMapping(value = "products")
+    public ResponseEntity saveProduct(@RequestBody ProductDTO productDTO) {
+        Product product = new Product(productDTO.getType(), productDTO.getName(), productDTO.getPrice(), productDTO.getQuantity());
+        return ResponseEntity.ok(productRepository.save(product));
+    }
+}
---
+++ src/main/java/com/onlineshoppingsystem/project/dto/ProductDTO.java
@@ -0,0 +1,12 @@
+package com.onlineshoppingsystem.project.dto;
+
+import lombok.Data;
+
+@Data
+public class ProductDTO {
+
+    private String type;
+    private String name;
+    private float price;
+    private int quantity;
+}
