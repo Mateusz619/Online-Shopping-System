1d00240b85db1a1782a3206a9e146f939efa5a2f
Arkadiusz <arek1990cda@gmail.com>
2021-04-23 17:09:27 +0200

    modify ProductController class + add CustomerDTO

--- src/main/java/com/onlineshoppingsystem/project/controller/ProductController.java
+++ src/main/java/com/onlineshoppingsystem/project/controller/ProductController.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
+
 @RestController
 public class ProductController {
 
@@ -29,5 +31,31 @@ public class ProductController {
     public ResponseEntity saveProduct(@RequestBody ProductDTO productDTO) {
         Product product = new Product(productDTO.getType(), productDTO.getName(), productDTO.getPrice(), productDTO.getQuantity());
         return ResponseEntity.ok(productRepository.save(product));
+    }
+
+    @PutMapping(value = "product/{id}")
+    public ResponseEntity updateProductById(@PathVariable Long id, @RequestBody ProductDTO productDTO) {
+        return productRepository.findById(id)
+                .map(product -> ResponseEntity.ok(saveProduct(product, productDTO)))
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    private Product saveProduct(Product product, ProductDTO productDTO) {
+        product.setType(productDTO.getType());
+        product.setName(productDTO.getName());
+        product.setPrice(productDTO.getPrice());
+        product.setQuantity(productDTO.getQuantity());
+
+        return productRepository.save(product);
+    }
+
+    @DeleteMapping(value = "product/{id}")
+    public ResponseEntity deleteProductById(@PathVariable Long id) {
+        Optional<Product> product = productRepository.findById(id);
+        if (product.isPresent()){
+            productRepository.delete(product.get());
+            return ResponseEntity.ok().build();
+        }
+        return ResponseEntity.notFound().build();
     }
 }
---
+++ src/main/java/com/onlineshoppingsystem/project/dto/CustomerDTO.java
@@ -0,0 +1,11 @@
+package com.onlineshoppingsystem.project.dto;
+
+import lombok.Data;
+
+@Data
+public class CustomerDTO {
+
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+}
